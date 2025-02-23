727a6abbd67ae208b0a4f1f103e32f161a74d2b5
badyan <bogdanwov@gmail.com>
2025-02-23 02:00:40 +0200

    add adaptive for main content

--- js/search-input.js
+++ js/search-input.js
@@ -1,13 +1,6 @@
-const query = window.matchMedia('(max-width: 860px)')
 let searchContainer = document.querySelector('.search-container')
 let searchBtn = searchContainer.querySelector('button');
 let searchInput = searchContainer.querySelector('input')
-
-// mediaQuery.addListener(() =>{
-//   if (query.matches){
-//     return // here change 
-//   }
-// })
 
 const togleAction = (action) =>{
   searchBtn.classList[action]("active")
@@ -16,16 +9,43 @@ const togleAction = (action) =>{
 }
 
 searchBtn.onclick = function () {
-  searchInput.focus()
   if (this.classList.contains('active')) { return } // here be search...
   
   togleAction('add')
 }
 
 searchInput.onblur = (event) =>{
-  if (event.relatedTarget === searchBtn) {
-    return
-  }
+  if (event.relatedTarget === searchBtn) { return }
   togleAction('remove')
 }
 
+searchBtn.onblur = (event) =>{
+  if (event.relatedTarget === searchInput) { return }
+  togleAction('remove')
+}
+
+const handleMediChange = () =>{
+  if (query.matches){
+    document.head.appendChild(Object.assign(document.createElement('link'), {
+      rel: 'stylesheet',
+      href: './css/search_input_after_860.css'
+    })) 
+  } else{
+    const link = document.querySelector('link[href="./css/search_input_after_860.css"]')
+    if (link){
+      link.remove()
+    }
+  }
+}
+
+let burgerBtn = document.querySelector('.burger-menu')
+let headerNav = document.querySelector('.header-nav')
+burgerBtn.onclick = function (){
+  this.classList.toggle('active')
+  headerNav.classList.toggle('active')
+}
+
+const query = window.matchMedia('(max-width: 860px)')
+query.addEventListener('change', handleMediChange)
+
+handleMediChange()
